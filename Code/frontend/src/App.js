44ee1d555c06736a9a3ee1b944de8195057fa7a7
Aryan Mishra <aryanmishracoc@gmail.com>
2023-11-02 13:58:17 +0530

    Pose Detection implemented

--- Code/frontend/src/App.js
+++ Code/frontend/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
+import Navbar from './components/Navbar'
 import Yoga from './pages/Yoga/Yoga'
+import Homer  from './pages/Home'
+import ExerciseDetail from './pages/ExerciseDetail'
 import './App.css'
 
 export default function App() {
   return (
     <Router>
+      <Navbar/>
       <Routes>
-        <Route path="/" element={<Yoga />} />
+        <Route path="/start" element={<Yoga />} />
+        <Route path="/" element={<Homer />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
       </Routes>
     </Router>
   );
